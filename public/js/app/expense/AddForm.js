9f3a6d9368f842d82bd64c844ad7939e67126577
Pavel Machekhin <pavel.machekhin@gmail.com>
2011-11-19 15:14:50 +0200

    Widgets for AddForm now is adding directly from the app.expense.AddForm to allow using of self writed widgets without implementation of ZF wrappers

--- public/js/app/expense/AddForm.js
+++ public/js/app/expense/AddForm.js
@@ -1,122 +1,126 @@
-dojo.provide('app.expense.AddForm');
+define([
+        "dojo/_base/declare", 
+        "dijit/form/Form",
+        "dijit/_WidgetsInTemplateMixin",
+        "dojo/text!./templates/AddForm.html",
+        "dojo/on",
+        "dijit/form/CheckBox",
+        "dijit/form/DateTextBox",
+        "dijit/form/TimeTextBox",
+        "dijit/form/FilteringSelect",
+        "dijit/form/Textarea",
+        "app/form/CurrencySpinner/CurrencySpinner",
+        "app/form/ValidationTextarea",
+        "app/Request",
+        
+        "dojo/domReady!"
+    ],
+    function(declare, Form, _WidgetsInTemplateMixin, template, on) {
+        return declare("app.expense.AddForm", [Form, _WidgetsInTemplateMixin], {
+            templateString: template,
+            
+            widgetsInTemplate: true,
+            
+            dateBox : false,
+        
+            isToday : false,
 
-dojo.require('dijit.form.Form');
-dojo.require('dijit.form.CheckBox');
-dojo.require('dijit.form.CurrencyTextBox');
-dojo.require('dijit.form.DateTextBox');
-dojo.require('dijit.form.TimeTextBox');
-dojo.require('dijit.form.FilteringSelect');
-dojo.require('dijit.form.NumberSpinner');
+            submitButton: false,
 
-dojo.declare(
-    'app.expense.AddForm', 
-    [dijit.form.Form], 
-    {
-        dateBox : false,
-        
-        isToday : false,
-        
-        submitButton: false,
-        
-        postCreate: function() {
-            this.connect(this, 'onSubmit', function(e) {
-                e.preventDefault();
+            options: options.spinner,
+
+            startup: function() {
+                var _dateBox = this.dateBox;
                 
-                this.submit();
-            });
-            
-            this.connect(this, 'onReset', function(e) {
-                this._reset();
-            });
-            
-            this.connect(this, 'startup', function(e) {
-                this.category = dijit.byId('category');
-                this.dateBox = dijit.byId('date');
-                this.isToday = dijit.byId('isToday');
-                this.submitButton = dijit.byId('submit');
-                
-                this.category.set('store', categoriesStore);
-                this.dateBox.set('disabled', true);
-            
-                this.connect(this.submitButton, 'onClick', this.submit);
-            
-                this.connect(this.isToday, 'onChange', function(value) {
+                this.selectToday.on('change', function(value) {
                     if (!value) {
-                        this.dateBox.set('disabled', false);
-                        
-                        this.dateBox.openDropDown();
+                        _dateBox.set('disabled', false);
+
+                        _dateBox.openDropDown();
                     } else {
-                        this.dateBox.set('disabled', true);
-                        this.dateBox.set('value', new Date());
-                        
-                        this.dateBox.closeDropDown();
+                        _dateBox.set('disabled', true);
+                        _dateBox.set('value', new Date());
+
+                        _dateBox.closeDropDown();
                     }
                 })
-            });
-        },
+            },
+
+            postCreate: function() {
+                this.options = options.spinner;
                 
-        _collectData: function() {
-            var data = this.get('value');
-            var options = { 
-                selector: "date", 
-                datePattern: 'yyyy-MM-dd'
-            };
-            
-            data['date'] = dojo.date.locale.format(this.dateBox.get('value'), options);
-            
-            return data;
-        },
-        
-        submit: function() {
-            if (!this.validate()) {
-                return false;
+                on(this.form, 'submit', dojo.hitch(this, '_submit'));
+
+                this.connect(this, 'onReset', function(e) {
+                    this._reset();
+                });
             }
+            , _collectData: function() {
+                var data = this.form.get('value');
+                var options = { 
+                    selector: "date", 
+                    datePattern: 'yyyy-MM-dd'
+                };
 
-            var form = this
-                , button = this.submitButton
-                , request = new app.Request();
+                data['date'] = dojo.date.locale.format(this.dateBox.get('value'), options);
 
-            button.set('disabled', true);
-
-            dojo.when(
-                request.post('/expense/add', this._collectData(), button)
-            ).then(function(response) {
-                if (1 == response.status) {
-                    form.reset();
-                    
-                    dojo.publish('/expense/add', [response.data]);
-                    dojo.publish('/app/info', [{type: 'message', message: response.message}]);
-                } else {
-                    dojo.publish('/app/message', [{type: 'message', message: response.message}]);
-                    
-                    dojo.forEach(form.getChildren(), function(input) {
-                        if (typeof response.errorMessages[input.get('name')] !== 'undefined') {
-
-                            input.focus();
-                            input.isValid(false);
-                            input.displayMessage(input.invalidMessage);
-                        }
-                    });
-                }                    
-            });
-            
-            return true;
-        },
-        
-        _reset: function() {
-            if (this.isToday) {
-                
-                setTimeout(function() {
-                    dijit.byId('category')._updatePlaceHolder();
-                }, 200);
-                
-                this.isToday.set('checked', 'checked');
-
-                this.dateBox.set('value', new Date());
-
-                this.dateBox.set('disabled', true);
-                
+                return data;
             }
-        }
+            , _submit: function(e) {
+                e.preventDefault();
+                
+                if (!this.validate()) {
+                    return false;
+                }
+
+                var form = this.form
+                    , button = this.submitButton
+                    , request = new app.Request();
+
+                button.set('disabled', true);
+
+                dojo.when(
+                    request.post('/expense/add', this._collectData(), button)
+                ).then(function(response) {
+                    if (1 == response.status) {
+                        form.reset();
+
+                        dojo.publish('/expense/add', [response.data]);
+                        dojo.publish('/app/info', [{type: 'message', message: response.message}]);
+                    } else {
+                        dojo.publish('/app/message', [{type: 'message', message: response.message}]);
+
+                        dojo.forEach(form.getChildren(), function(input) {
+                            if (typeof response.errorMessages[input.get('name')] !== 'undefined') {
+
+                                input.focus();
+
+                                if (dojo.isFunction(input.isValid)) {
+                                    input.isValid(false);
+
+                                    input.displayMessage(input.invalidMessage);
+                                } else {
+                                    dijit.Tooltip.show('message', input.domNode);
+
+                                    input.on('blur', function() {
+                                        dijit.Tooltip.hide(input.domNode);
+                                    })
+
+
+                                }
+
+                            }
+                        });
+                    }                    
+                });
+
+                return true;
+            }
+            , _reset: function() {
+                if (this.dateBox) {
+                    this.selectToday.set('checked', 'checked');
+                }
+            }
+        });
     }
 );
