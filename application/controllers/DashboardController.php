e2cc7efe3536e191077f17aa9ff6d537a7ddb50d
Pavel Machekhin <pavel.machekhin@gmail.com>
2011-12-28 23:20:20 +0200

    Added dashboard page as default tab

---
+++ application/controllers/DashboardController.php
@@ -0,0 +1,13 @@
+<?php
+
+class DashboardController extends Zend_Controller_Action {
+
+    public function init() {
+        $this->_helper->layout()->disableLayout();
+    }
+
+    public function indexAction() {
+        
+    }
+}
+
